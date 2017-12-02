df18e6a1d7e1086cfca0803e77df282a09a36b8f
DESKTOP-VNO2FDQ\Anton <antondan.tuc@gmail.com>
2017-12-02 18:34:12 +0200

    Fully Working Trippy Effect (not only left side)

---
+++ TrippyDrive/Assets/Antonis/MovingCar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCar : MonoBehaviour {
+
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
